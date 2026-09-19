4aca66370b5b2d11d45875e9a86efe1663b185d7
Amrit Ganesh <aganesh2@usf.edu>
2026-09-19 15:57:43 -0400

    chore: sync openapi contract to match CI generated schema

--- frontend/src/api.generated.ts
+++ frontend/src/api.generated.ts
@@ -924,6 +924,10 @@ export interface components {
             msg: string;
             /** Error Type */
             type: string;
+            /** Input */
+            input?: unknown;
+            /** Context */
+            ctx?: Record<string, never>;
         };
     };
     responses: never;
